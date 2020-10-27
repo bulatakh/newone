77790fd019fcbc74073da0f135aaaf12f09fc732
Bulat <bulat.a@mail.ru>
2020-10-27 21:25:50 +0300

    практика ч1

--- script/script.js
+++ script/script.js
@@ -1,6 +1,17 @@
 "use strict";
+const numberOfFilms = prompt("Сколько уже посмотрели?", "");
+const personalMovieDB = {  
+        count : numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        private : false
+};
 
-const a = true,
-      b = false;
-
-console.log(a || b);
+const a = prompt("Последний просмотренный фильм?", ""),
+      b = prompt("На сколько оцените его", ""),
+      c = prompt("Последний просмотренный фильм?", ""),
+      d = prompt("На сколько оцените его", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
